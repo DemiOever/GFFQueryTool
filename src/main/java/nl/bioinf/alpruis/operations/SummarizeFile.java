7cef06f683f3bfc40a0269b0835e6b288ac5dd58
Alphina18 <aimeepruis@outlook.com>
2024-10-04 18:17:09 +0200

    renamed the filename for the class to meet the java class name requirements. And started with making some methods for the summary.

---
+++ src/main/java/nl/bioinf/alpruis/operations/SummarizeFile.java
@@ -0,0 +1,28 @@
+package nl.bioinf.alpruis.operations;
+
+public class SummarizeFile {
+    private static int gettingLengthSequence() {
+        // length of the sequence
+        return 14;
+    }
+    private static int gettingGcPercentage() {
+    // gc-percentage
+        return 100;
+    }
+
+    private static void countingFeatureTypes() {
+    // feature types with the amount present
+    }
+
+    private static void averageLengthGenes() {
+    // Average lengths of the genes
+    }
+
+    private static void countForwardStrands() {
+    // number on forward strand
+    }
+
+    private static void countReverseStrands() {
+    // number on reverse strand
+    }
+}
