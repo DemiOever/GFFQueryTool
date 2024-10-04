bad8a0f84d88aba049f0d2ad4a6f6c3ba5f07c91
Alphina18 <aimeepruis@outlook.com>
2024-10-04 18:31:14 +0200

    added some methods to ReturningFile and added updated log.

--- src/main/java/nl/bioinf/alpruis/ReturningFile.java
+++ src/main/java/nl/bioinf/alpruis/ReturningFile.java
@@ -1,4 +1,21 @@
 package nl.bioinf.alpruis;
 
 public class ReturningFile {
+    // fasta return
+    private static void returnFasta(){
+
+    }
+
+    // gff return
+    private static void returnGff(){
+
+    }
+
+    private static void returnTxt(){
+
+    }
+
+    private static void returnCsv(){
+
+    }
 }
