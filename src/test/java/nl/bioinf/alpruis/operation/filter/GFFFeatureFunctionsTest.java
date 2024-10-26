e4c950f4423f7d7c77e813b6308cd48a75806400
Alphina18 <aimeepruis@outlook.com>
2024-10-26 12:54:02 +0200

    finished the tests for GFFFeatureFunctions except for Attributes

--- src/test/java/nl/bioinf/alpruis/operation/filter/GFFFeatureFunctionsTest.java
+++ src/test/java/nl/bioinf/alpruis/operation/filter/GFFFeatureFunctionsTest.java
@@ -44,38 +44,74 @@ public class GFFFeatureFunctionsTest {
     public void testType() {
         Feature feature = makeFeature();
         assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("region"), false, false));
-        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene", "exon"), false, false));
-        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gen"), false, true));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("region", "exon"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("region"), false, true));
         assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene"), true, false)); // because when it isn't found it returns true
-        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene"), true, true)); // because when it isn't found it returns true
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gen"), true, true)); // because when it isn't found it returns true
 
         assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("intron"), false, false));
         assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("genes", "intron"), false, false));
         assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene"), false, true));
-        assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene"), true, false)); // because when it needs to be deleted it returns false
-        assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("gene"), true, true)); // because when it needs to be deleted it returns false
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("region"), true, false)); // because when it needs to be deleted it returns false
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "TYPE", List.of("regi"), true, true)); // because when it needs to be deleted it returns false
 
     }
 
     @Test
     public void testChromosome() {
         Feature feature = makeFeature();
-        boolean filterTrue = GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067.7"), false, false);
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067.7"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_00006", "NC_000067.7"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067"), false, true));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000068"), true, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000068"), true, true));
 
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000068.8"), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067", "NC_00067.7"), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_00007"), false, true));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067.7"), true, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "CHROMOSOME", List.of("NC_000067"), true, true));
     }
 
     @Test
     public void testRegion() {
+        // 2 8
         Feature feature = makeFeature();
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("1", "10"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("1", "10", "2", "20"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("5", "10"), false, true));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("5", "10"), true, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("9", "12"), true, true));
 
-        boolean filterTrue = GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("5", "10"), false, true);
-        boolean filterFalse = GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("1", "10"), false, false);
-
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("5", "10"), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("5", "10", "1", "7"), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("9", "15"), false, true));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("1", "10"), true, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "REGION", List.of("1", "6"), true, true));
     }
 
     @Test
     public void testSource() {
+        Feature feature = makeFeature();
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeq"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeq","BestRefSeq%2CGnomon"), false, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeq"), false, true));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeqFE"), true, false));
+        assertTrue(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeqFE"), true, true));
 
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeqFE"), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeqFE",""), false, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeqFE"), false, true));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("RefSeq"), true, false));
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "SOURCE", List.of("Ref"), true, true));
     }
+
+    @Test
+    public void testColumn(){
+        Feature feature = makeFeature();
+        assertFalse(GFFFeatureFunctions.filteringLine(feature, "UNKNOWN", List.of("RefSeq"), false, false));
+    }
+
+    //test for attributes
 
 }
